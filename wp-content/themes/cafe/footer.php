e4a39cb9d810104eb43d87c9c4da7000be80e222
d1av <davi4alves@gmail.com>
2023-04-28 21:31:52 -0300

    feat: :sparkles: add header correct

---
+++ wp-content/themes/cafe/footer.php
@@ -0,0 +1,69 @@
+	<footer id="footer" class="tf_box pagewidth tf_scrollbar tf_rel tf_clearfix" itemscope="itemscope" itemtype="https://schema.org/WPFooter">
+		<!-- Footer code Start -->
+		<?php wp_footer() ?>
+		<!-- Footer code End -->
+		<div class="back-top tf_textc tf_clearfix back-top-float">
+			<div class="arrow-up"><a aria-label="Back to top" href="#header"><span class="screen-reader-text">Back To Top</span></a></div>
+		</div>
+		<div class="main-col first tf_clearfix">
+			<div class="footer-left-wrap first">
+				<div class="footer-logo-wrapper tf_clearfix">
+					<div id="footer-logo"><a href="https://themify.org/ultra-coffee" title="Coffee"><span>Coffee</span></a></div>
+				</div>
+			</div>
+			<div class="footer-right-wrap">
+				<div class="footer-nav-wrap"></div>
+			</div>
+		</div>
+		<div class="section-col tf_clearfix">
+			<div class="footer-widgets-wrap">
+				<div class="footer-widgets tf_clearfix">
+					<div class="col4-1 first tf_box tf_float">
+						<div id="text-1002" class="widget widget_text">
+							<h4 class="widgettitle">Coffee</h4>
+							<div class="textwidget">
+								<p>We’re excited to share the story that every bean can tell. Come on in, take a sip, and explore with us!</p>
+							</div>
+						</div>
+					</div>
+					<div class="col4-1 tf_box tf_float">
+						<div id="text-1003" class="widget widget_text">
+							<h4 class="widgettitle">Hours</h4>
+							<div class="textwidget">
+								<p>MON – FRI: 9AM – 11PM<br>
+									SAT – SUN: 5PM – 2AM<br>
+									Bar open only on weekends.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col4-1 tf_box tf_float">
+						<div id="text-1004" class="widget widget_text">
+							<h4 class="widgettitle">Contact</h4>
+							<div class="textwidget">
+								<p>55 Adelaide St E,<br>
+									Toronto, ON, M5C 1K6<br>
+									Canada<br>
+									<a href="mailto:info@themify.me">info@coffee-skin.com</a>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col4-1 tf_box tf_float"></div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-text tf_clear tf_clearfix">
+			<div class="footer-text-inner">
+				<div class="one">© <a href="https://themify.org/ultra-coffee">Coffee</a> 2023</div>
+				<div class="two">Powered by <a href="http://wordpress.org/">WordPress</a> • <a href="https://themify.me/">Themify WordPress Themes</a></div>
+			</div>
+		</div>
+	</footer>
+	</div>
+	</div>
+
+
+	<div class="body-overlay"></div>
+	</body>
+
+	</html>
--- wp-content/themes/cafe/header.php
+++ wp-content/themes/cafe/header.php
@@ -25,36 +25,71 @@
     <!-- Header Scripts End -->
 </head>
 
-<header id="header" class="tf_box pagewidth tf_clearfix" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
-    <div class="header-bar tf_box">
-        <div id="site-logo"><a href="https://themify.org/ultra-coffee" title="Coffee"><span>Coffee</span></a></div>
-    </div>
-    <div id="mobile-menu" class="sidemenu sidemenu-off tf_scrollbar">
-        <div class="navbar-wrapper tf_clearfix">
-            <div class="cart-icon empty-cart">
-                <div class="cart-wrap"><a class="cart-icon-link" href="https://themify.org/ultra-coffee/#slide-cart"><em class="icon-shopping-cart"><svg aria-label="Shop Cart" class="tf_fa tf-fas-shopping-cart" role="img">
-                                <use href="#tf-fas-shopping-cart"></use>
-                            </svg></em><span></span><em class="screen-reader-text">Cart</em></a></div>
-            </div>
-            <div class="social-widget tf_inline_b tf_vmiddle"> </div><a class="search-button tf_search_icon tf_vmiddle tf_inline_b" href="https://themify.org/ultra-coffee/#"><svg aria-label="Search" class="tf_fa tf-fas-search" role="img">
-                    <use href="#tf-fas-search"></use>
-                </svg><span class="screen-reader-text">Search</span></a>
-            <nav id="main-nav-wrap" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement">
-                <ul id="main-nav" class="main-nav tf_clearfix tf_box" data-init="true">
-                    <li class="menu-item-page-60 menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-60 current_page_item menu-item-76"><a href="https://themify.org/ultra-coffee/">Home</a> </li>
-                    <li class="menu-item-page-62 menu-item menu-item-type-post_type menu-item-object-page menu-item-75"><a href="https://themify.org/ultra-coffee/about/">About</a> </li>
-                    <li class="menu-item-page-8 menu-item menu-item-type-post_type menu-item-object-page menu-item-74"><a href="https://themify.org/ultra-coffee/shop/">Shop</a> </li>
-                    <li class="menu-item-page-61 menu-item menu-item-type-post_type menu-item-object-page menu-item-73"><a href="https://themify.org/ultra-coffee/contact/">Contact</a> </li>
-                </ul>
-            </nav>
-        </div><a id="menu-icon-close" aria-label="Close menu" class="tf_close tf_hide" href="https://themify.org/ultra-coffee/#"><span class="screen-reader-text">Close Menu</span></a>
-    </div>
-    <div id="slide-cart" class="sidemenu sidemenu-off tf_scrollbar"><a href="https://themify.org/ultra-coffee/#" id="cart-icon-close" class="tf_close"><span class="screen-reader-text">Close Cart</span></a>
-        <div id="shopdock-ultra">
 
-            Your cart is empty. Go to <a href="https://themify.org/ultra-coffee/shop/">Shop</a>.
+<body class="home page-template-default page page-id-60 theme-themify-ultra skin-coffee sidebar-none full_width woocommerce-js tb_animation_on ready-view header-horizontal fixed-header-enabled footer-horizontal-left tagline-off filter-hover-none filter-featured-only slide-cart sidemenu-active fixed-header-on" style="">
+    <a class="screen-reader-text skip-to-content" href="#content">Skip to content</a>
+
+
+    <div class="tf_search_form tf_search_overlay">
+        <form role="search" method="get" id="searchform" class="tf_rel tf_hide" action="https://themify.org/ultra-coffee/">
+            <div class="tf_icon_wrap icon-search">
+                <svg aria-label="Search" class="tf_fa tf-ti-search" role="img">
+                    <use href="#tf-ti-search"></use>
+                </svg>
+            </div>
+            <input type="text" name="s" id="s" title="Search" placeholder="Search" value="">
+        </form>
+    </div>
+    <div id="pagewrap" class="tf_box hfeed site">
+        <div id="headerwrap" class="tf_box tf_w fixed-header">
+            <div class="header-icons tf_hide"><a id="menu-icon" class="tf_inline_b tf_text_dec" href="https://themify.org/ultra-coffee/#mobile-menu" aria-label="Menu"><span class="menu-icon-inner tf_inline_b tf_vmiddle tf_overflow"></span><span class="screen-reader-text">Menu</span></a>
+                <div class="cart-icon empty-cart">
+                    <div class="cart-wrap">
+                        <a class="cart-icon-link" href="https://themify.org/ultra-coffee/#slide-cart">
+                            <em class="icon-shopping-cart">
+                                <svg aria-label="Shop Cart" class="tf_fa tf-fas-shopping-cart" role="img">
+                                    <use href="#tf-fas-shopping-cart">
+                                    </use>
+                                </svg>
+                            </em>
+
+                            <em class="screen-reader-text">Cart</em></a>
+                    </div>
+                </div>
+            </div>
+
+
+            <header id="header" class="tf_box pagewidth tf_clearfix" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
+                <div class="header-bar tf_box">
+                    <div id="site-logo"><a href="https://themify.org/ultra-coffee" title="Coffee"><span>Coffee</span></a></div>
+                </div>
+                <div id="mobile-menu" class="sidemenu sidemenu-off tf_scrollbar">
+                    <div class="navbar-wrapper tf_clearfix">
+                        <div class="cart-icon empty-cart">
+                            <div class="cart-wrap"><a class="cart-icon-link" href="https://themify.org/ultra-coffee/#slide-cart"><em class="icon-shopping-cart"><svg aria-label="Shop Cart" class="tf_fa tf-fas-shopping-cart" role="img">
+                                            <use href="#tf-fas-shopping-cart"></use>
+                                        </svg></em><span></span><em class="screen-reader-text">Cart</em></a></div>
+                        </div>
+                        <div class="social-widget tf_inline_b tf_vmiddle"> </div><a class="search-button tf_search_icon tf_vmiddle tf_inline_b" href="https://themify.org/ultra-coffee/#"><svg aria-label="Search" class="tf_fa tf-fas-search" role="img">
+                                <use href="#tf-fas-search"></use>
+                            </svg><span class="screen-reader-text">Search</span></a>
+                        <nav id="main-nav-wrap" itemscope="itemscope" itemtype="https://schema.org/SiteNavigationElement">
+                            <ul id="main-nav" class="main-nav tf_clearfix tf_box" data-init="true">
+                                <li class="menu-item-page-60 menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-60 current_page_item menu-item-76"><a href="https://themify.org/ultra-coffee/">Home</a> </li>
+                                <li class="menu-item-page-62 menu-item menu-item-type-post_type menu-item-object-page menu-item-75"><a href="https://themify.org/ultra-coffee/about/">About</a> </li>
+                                <li class="menu-item-page-8 menu-item menu-item-type-post_type menu-item-object-page menu-item-74"><a href="https://themify.org/ultra-coffee/shop/">Shop</a> </li>
+                                <li class="menu-item-page-61 menu-item menu-item-type-post_type menu-item-object-page menu-item-73"><a href="https://themify.org/ultra-coffee/contact/">Contact</a> </li>
+                            </ul>
+                        </nav>
+                    </div><a id="menu-icon-close" aria-label="Close menu" class="tf_close tf_hide" href="https://themify.org/ultra-coffee/#"><span class="screen-reader-text">Close Menu</span></a>
+                </div>
+                <div id="slide-cart" class="sidemenu sidemenu-off tf_scrollbar"><a href="https://themify.org/ultra-coffee/#" id="cart-icon-close" class="tf_close"><span class="screen-reader-text">Close Cart</span></a>
+                    <div id="shopdock-ultra">
+
+                        Your cart is empty. Go to <a href="https://themify.org/ultra-coffee/shop/">Shop</a>.
+
+                    </div>
+                </div>
+            </header>
 
         </div>
-        <!-- /#shopdock -->
-    </div>
-</header>
